2a216396114764970771195583211aeb67ad065b
Chris Montgomery <chr.montgomery@gmail.com>
2015-02-25 10:47:45 -0600

    smarter user details page to account for time periods

--- src/client/js/filters.js
+++ src/client/js/filters.js
@@ -9,4 +9,31 @@
     }
   }]);
 
+  module.filter('i18n', ['localizedTexts', function (localizedTexts) {
+    return function (text) {
+      if (localizedTexts.hasOwnProperty(text)) {
+        return localizedTexts[text];
+      }
+      return text;
+    };
+  }]);
+
+  // this can be in a seperate file, but should load after the filter is loaded
+  module.value('localizedTexts', {
+    'allTime': 'All Time',
+    'sixMonths': 'Last 6 Months',
+    'thisMonth': 'Last 30 days',
+    'thisWeek': 'Last 7 days',
+    'winPercentage': 'Win Percentage',
+    'wins': 'Wins',
+    'losses': 'Losses',
+    'second': 'Seconds',
+    'third': 'Thirds',
+    'fourth': 'Fourths',
+    'fifth': 'Fifth',
+    'sixth': 'Sixths',
+    'ties': 'Ties',
+    'total': 'Played'
+  });
+
 })();
